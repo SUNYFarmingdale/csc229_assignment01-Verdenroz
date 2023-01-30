511d6c474a6a866df688635043de9fe9e90a2dd5
Harvey Tseng <tsenh@farmingdale.edu>
2023-01-30 13:51:27 -0500

    Finished assignment

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -22,7 +22,8 @@ The Course class should have:
  - Push the code to GitHub.
 
  - Submit the Github link here (make sure it is a public repo).
-
+    Harvey Tseng
+    https://github.com/SUNYFarmingdale/csc229_assignment01-Verdenroz.git
 
  */
 
@@ -33,5 +34,52 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class Course {
-
+    //Instance fields
+    private String ID;
+    private String Name;
+    private String Code;
+    
+    //Default constructor
+    public Course(){
+        ID = "R02112686";
+        Name = "Harvey Tseng";
+        Code = "CSC 229";
+    }
+    
+    //Overloaded constructor -- create Course object with parameters
+    public Course(String ID, String Name, String Code){
+        this.ID = ID;
+        this.Name = Name;
+        this.Code = Code;
+    }
+    
+    // change ID
+    public void setID(String id){
+        ID = id;
+    }
+    
+    //change Name
+    public void setName(String name){
+        Name = name;
+    }
+    
+    //change Code
+    public void setCode(String code){
+        Code = code;
+    }
+    
+    // Get ID
+    public String getID(){
+        return ID;
+    }
+    
+    // Get Name
+    public String getName(){
+        return Name;
+    }
+    
+    //Get Cod
+    public String getCode(){
+        return Code;
+    }
 }
